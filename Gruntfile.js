52807abafa67370aa217c25d00abb7d3018548d0
Zach Wolf <hello@zachwolf.com>
2013-12-28 14:04:16 -0600

    Grunt compass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,49 @@
 /*global module:false*/
+
+var SOURCE_PATH = "/source",
+    BUILD_PATH = "/build";
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     // Task configuration.
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {}
+    compass: {
+      dist: {
+        options: {
+          outputStyle: 'compressed',
+          noLineComments: true
+        }
       },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
+      dev: {
+        options: {
+          outputStyle: 'expanded',
+          watch: false
+        }
       }
-    },
-    qunit: {
-      files: ['test/**/*.html']
     },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
+        tasks: ['compass:dev']
       }
+      // ,
+      // lib_test: {
+      //   files: '<%= jshint.lib_test.src %>',
+      //   tasks: ['jshint:lib_test', 'qunit']
+      // }
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-qunit');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit']);
+  // grunt.registerTask('default', ['jshint', 'qunit']);
+  grunt.registerTask('dev', ['compass:dev']);
+  grunt.registerTask('default', ['compass:dist']);
 
 };
